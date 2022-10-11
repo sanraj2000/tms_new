89c1d98fb6872df5d7dd134deb693b45f2207deb
sanraj2000 <70739919+sanraj2000@users.noreply.github.com>
2022-10-11 12:51:21 +0530

    Update main.js

--- ui/js/main.js
+++ ui/js/main.js
@@ -983,3 +983,21 @@ $http(awtest1).then(function(response2) {
             document.getElementById(tabName).style.display = "block";
             evt.currentTarget.className += " active";
           }
+
+
+
+
+function connectorTabsv(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontentv");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tv tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
